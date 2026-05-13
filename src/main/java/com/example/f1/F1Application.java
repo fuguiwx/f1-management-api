1113f181d5d31ad08a08c7cd7ba794176e90d650
fuguiwx <hhhhweixin2369@qq.com>
2026-05-13 17:46:32 +0800

    debug: add db config logging

--- src/main/java/com/example/f1/F1Application.java
+++ src/main/java/com/example/f1/F1Application.java
@@ -3,12 +3,19 @@ package com.example.f1;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @MapperScan("com.example.f1.mapper")
 public class F1Application {
     public static void main(String[] args) {
-        SpringApplication.run(F1Application.class, args);
-
+        ConfigurableApplicationContext ctx = SpringApplication.run(F1Application.class, args);
+        Environment env = ctx.getEnvironment();
+        System.out.println("===== DB CONFIG =====");
+        System.out.println("URL: " + env.getProperty("spring.datasource.url"));
+        System.out.println("USER: " + env.getProperty("spring.datasource.username"));
+        System.out.println("PASS: " + env.getProperty("spring.datasource.password"));
+        System.out.println("====================");
     }
 }
